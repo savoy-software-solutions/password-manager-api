39f03edaaea2a58e85b22ad46f618cc32f03d427
John Savoy <savoy.software.solutions@gmail.com>
2026-03-22 23:37:44 -0400

    initial commit: spring boot project setup with spring security and JWT

---
+++ src/main/java/com/johnsavoy/password_manager_api/PasswordManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.johnsavoy.password_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnsavoy/password_manager_api/PasswordManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnsavoy.password_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
